a0f367cca94f9977d78991be98546ecea5e6ce16
JesusFpz <jfplaz@gmail.com>
2024-08-27 11:22:50 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//write your code here
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function Generate(array) {
+  return Math.floor(Math.random() * array.length);
+}
+function MixExcuses() {
+  let whoElement = who[Generate(who)];
+  let actionElement = action[Generate(action)];
+  let whatElement = what[Generate(what)];
+  let whenElement = when[Generate(when)];
+
+  let excuses = `${whoElement} ${actionElement} ${whatElement} ${whenElement}`;
+  console.log(excuses);
+  return excuses;
+}
+
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+
+  let excusagenerated = "MixExcuses";
+  document.getElementById("excuse").innerText =
+    document.getElementById("excuse").innerText || MixExcuses();
+
+  document.querySelector("button").addEventListener("click", function() {
+    let excusagenerated = MixExcuses();
+    document.getElementById("excuse").innerText = excusagenerated;
+  });
 };
